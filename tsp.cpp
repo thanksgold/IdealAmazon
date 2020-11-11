72f0fed9199e2affecddcb684100a525fea28d7d
Rakhmatilla Bakhodirov <u6457384@anu.edu.au>
2020-11-11 20:22:07 +1100

    Updated tsp

--- tsp.cpp
+++ tsp.cpp
@@ -1,43 +1,104 @@
 #include <vector>
 #include <iostream>
+#include <cmath>
 
-using namespace std;
+#define V std::vector
 
-int tsp(const vector<vector<int>>& cities, int pos, int visited, vector<vector<int>>& state)
-{
-    if(visited == ((1 << cities.size()) - 1))
-        return cities[pos][0]; // return to starting city
-
-    if(state[pos][visited] != INT_MAX)
-        return state[pos][visited];
-
-    for(int i = 0; i < cities.size(); ++i)
-    {
-        // can't visit ourselves unless we're ending & skip if already visited
-        if(i == pos || (visited & (1 << i)))
-            continue;
-
-        int distance = cities[pos][i] + tsp(cities, i, visited | (1 << i), state);
-        if(distance < state[pos][visited])
-            state[pos][visited] = distance;
+struct ReturnPath {
+    int length;
+    V<int> path;
+    // constructor for ReturnPath struct
+    ReturnPath(int length_, V<int> path_) {
+        length = length_;
+        path = path_;
     }
+};
 
-    return state[pos][visited];
+struct Point {
+    double x, y;
+    // constructor for Point struct
+    Point(double x_, double y_) {
+        x = x_;
+        y = y_;
+    }
+};
+
+double euclideanDistance(Point p1, Point p2) {
+    double x = p1.x - p2.x;
+    double y = p1.y - p2.y;
+    return sqrt(x*x + y*y);
 }
 
-int main()
-{
-    vector<vector<int>> cities = { { 0, 20, 42, 35 },
-                                   { 20, 0, 30, 34 },
-                                   { 42, 30, 0, 12 },
-                                   { 35, 34, 12, 0 }
-                                 };
+int printV(V<V<ReturnPath>> A) {
+    std::cout << "\n";
+    for (auto r : A) {
+        std::cout << "{ ";
+        for (auto c : r) {
+            std::cout << "{ ";
+            for (auto z : c.path)
+                std::cout << z << " ";
+            if (c.length == INT_MAX)
+                std::cout << "}max ";
+            else
+                std::cout << "}" << c.length << " ";
+        }
+        std::cout << "}\n";
+    }
+}
 
-    vector<vector<int>> state(cities.size());
-    for(auto& neighbors : state)
-        neighbors = vector<int>((1 << cities.size()) - 1, INT_MAX);
+int print(ReturnPath rtn) {
+    std::cout << "shortest distance for { ";
+    for (auto r : rtn.path) {
+            std::cout << r << " ";
+    }
+    std::cout << "} is " << rtn.length << "\n";
+}
 
-    cout << "minimum: " << tsp(cities, 0, 1, state) << endl;
+ReturnPath tsp(const V<V<int>>& distances, int position, int visited, V<V<ReturnPath>>& state) {
+    // if reached the end point then return the distance to the first
+    if (visited == ((1 << distances.size()) - 1))
+        return ReturnPath(distances[position][0], V<int>(1,distances[position][0]));
+    // if it has already been calculated, then use that
+    if (state[position][visited].length != INT_MAX)
+        return state[position][visited];
+    for (int i = 0; i < distances.size(); ++i) {
+        if(i == position || (visited & (1 << i)))
+            continue;
+        // calculate next route
+        ReturnPath r = tsp(distances, i, visited | (1 << i), state);
+        // if the path is shorter then update
+        if (distances[position][i] + r.length < state[position][visited].length) {
+            state[position][visited].length = distances[position][i] + r.length;
+            if (!state[position][visited].path.empty())
+                state[position][visited].path.clear();
+            state[position][visited].path.push_back(distances[position][i]);
+            for (int j = 0; j < r.path.size(); ++j)
+                state[position][visited].path.push_back(r.path[j]);
+        }
+    }
+    return state[position][visited];
+}
+
+int main() {
+    V<V<int>> distances = { { 0, 10, 15, 20 },
+                            { 10, 0, 35, 25 },
+                            { 15, 35, 0, 30 },
+                            { 20, 25, 30, 0 } };
+
+    V<V<int>> distances2 = { { 0, 20, 42, 35 },
+                             { 20, 0, 30, 34 },
+                             { 42, 30, 0, 12 },
+                             { 35, 34, 12, 0 }
+                            };
+
+    V<ReturnPath> type = V<ReturnPath>((1 << distances.size()) - 1, ReturnPath(INT_MAX, V<int>()));
+    V<V<ReturnPath>> state(distances.size(), type);
+
+    ReturnPath r = tsp(distances, 0, 1, state);
+
+    print(r);
+
+    // std::cout << "euclidean:" << euclideanDistance(Point(0, 0), Point(12, 16));
 
     return 0;
 }
